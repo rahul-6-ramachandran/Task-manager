f9a55c65e7c977e84f36f44058c3cab6ad0f03e0
rahul_r_432 <rahulramachandran432@gmail.com>
2025-04-02 21:08:46 +0530

    feat : frontend backend initial and final docker setup

--- server/index.ts
+++ server/index.ts
@@ -6,11 +6,11 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
-
+const port = process.env.PORT || 7070;
 app.get('/',(req:any,res : any)=>{
     res.send("Hello World")
 })
 
-app.listen(process.env.PORT,() => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.listen(port,() => {
+    console.log(`Server running on port ${port}`);
   })
