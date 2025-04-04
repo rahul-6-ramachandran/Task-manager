dc50790ec5af8664a75e091a14145af1a9150f72
rahul_r_432 <rahulramachandran432@gmail.com>
2025-04-05 01:11:28 +0530

    fix : edit delete button gap

--- client/src/components/task/Taskiew.tsx
+++ client/src/components/task/Taskiew.tsx
@@ -84,7 +84,7 @@ export default function Taskview(){
                  { new Date(task.updatedAt).toLocaleString()}
                 </td>
                 <td className="px-6 py-4">
-                  <div className="px-3">
+                  <div className="px-3 w-full flex">
                   <button type="button" className="text-white px-2 bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
                   <button type="button"  onClick={()=>handleEditClick(task)} className="text-white px-2 bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</button>
                   </div>
